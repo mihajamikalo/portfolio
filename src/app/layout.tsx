13938bb63da8106eaf5351d4feb4cc07cab88462
Tiavina Liantsoa <Mihajamikalo@gmail.com>
2026-05-15 17:59:06 +0300

    adding speed monitor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import LanguageToggleFloating from "@/components/language-toggle-floating";
 import SiteFooter from "@/components/site-footer";
@@ -47,6 +48,7 @@ export default async function RootLayout({
         </main>
         <SiteFooter locale={locale} />
         <LanguageToggleFloating locale={locale} />
+        <SpeedInsights />
       </body>
     </html>
   );
